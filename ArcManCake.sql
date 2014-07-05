663423fc096282c27d35465cda29563a7fa1f530
ricargoes <ricardogomezescalante@gmail.com>
2014-07-05 17:09:47 +0200

    UML and sql dump

--- ArcManCake.sql
+++ ArcManCake.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 29, 2014 at 02:48 
+-- Generation Time: Jul 04, 2014 at 08:58 
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -23,32 +23,91 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `costumers`
+-- Table structure for table `bought_extras`
 --
 
-CREATE TABLE IF NOT EXISTS `costumers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `surname` varchar(20) NOT NULL,
-  `notes` text,
-  `phone` varchar(20) NOT NULL,
-  `email` varchar(20) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `bought_extras` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `price` int(11) NOT NULL,
+  `factor` int(11) NOT NULL,
+  `extra_id` int(11) DEFAULT NULL,
+  `proposal_id` int(11) DEFAULT NULL,
   `created` date DEFAULT NULL,
   `modified` date DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
--- Dumping data for table `costumers`
+-- Dumping data for table `bought_extras`
 --
 
-INSERT INTO `costumers` (`id`, `name`, `surname`, `notes`, `phone`, `email`, `created`, `modified`, `user_id`) VALUES
-(1, 'Stefan', 'Brakhane', '', '1234', 's@no.com', '2014-06-01', '2014-06-12', 1),
-(3, 'Jonatan', 'Zopes', 'Joooooooooooooonatan', '4321', '', '2014-06-04', '2014-06-12', 2),
-(5, 'Carla', 'Hallabrin', '^^', '666', 'nu@ha.com', '2014-06-16', '2014-06-16', 2),
-(6, 'costum', 'a', 'for a', '456789', '', '2014-06-16', '2014-06-16', 4),
-(7, 'costum ', 'a 2', 'aedf', '35', '', '2014-06-16', '2014-06-16', 4);
+INSERT INTO `bought_extras` (`id`, `price`, `factor`, `extra_id`, `proposal_id`, `created`, `modified`) VALUES
+(1, 100000, 1, 14, 2, '2014-07-04', '2014-07-04'),
+(2, 765, 1, 15, 2, '2014-07-04', '2014-07-04'),
+(3, 8765, 1, 13, 2, '2014-07-04', '2014-07-04'),
+(4, 3, 1, 16, 2, '2014-07-04', '2014-07-04'),
+(5, 1050, 1, 17, 2, '2014-07-04', '2014-07-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` date DEFAULT NULL,
+  `modified` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `user_id`, `created`, `modified`) VALUES
+(1, 'Kitchen', 3, '2014-07-02', '2014-07-02'),
+(2, 'Lair', 3, '2014-07-02', '2014-07-02'),
+(3, 'Office', 3, '2014-07-02', '2014-07-02'),
+(4, 'Bedroom', 4, '2014-07-02', '2014-07-02'),
+(5, 'Living room', 4, '2014-07-02', '2014-07-02'),
+(6, 'Garden', 6, '2014-07-04', '2014-07-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `surname` varchar(200) NOT NULL,
+  `notes` text NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `address1` varchar(200) NOT NULL,
+  `address2` varchar(200) NOT NULL,
+  `zipcode` int(11) NOT NULL,
+  `city` varchar(200) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` date DEFAULT NULL,
+  `modified` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `surname`, `notes`, `phone`, `email`, `address1`, `address2`, `zipcode`, `city`, `user_id`, `created`, `modified`) VALUES
+(1, 'cus', 'a', '', 2147483647, 'dhf@kjaefh.com', '', '', 0, '', 3, '2014-07-01', '2014-07-01'),
+(2, 'cus', 'a2', '', 1631728215, 'sfvn@jsn.com', '', '', 0, '', 3, '2014-07-01', '2014-07-01'),
+(3, 'cus', 'b', '', 23456789, 'yasgevb@hgsjb.com', '', '', 0, '', 4, '2014-07-01', '2014-07-01'),
+(4, 'cus', 'b2', '', 45689, 'akejnm@kand.com', '', '', 0, '', 4, '2014-07-01', '2014-07-01'),
+(5, 'cus', 'own', '', 679, 'sdjf@ikshf.com', '', '', 0, '', 2, '2014-07-01', '2014-07-01');
 
 -- --------------------------------------------------------
 
@@ -58,35 +117,66 @@ INSERT INTO `costumers` (`id`, `name`, `surname`, `notes`, `phone`, `email`, `cr
 
 CREATE TABLE IF NOT EXISTS `extras` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` int(11) NOT NULL,
-  `notes` text NOT NULL,
-  `price` int(11) NOT NULL,
-  `factorA` int(11) NOT NULL,
-  `factorB` int(11) NOT NULL,
-  `factorC` int(11) NOT NULL,
-  `factorD` int(11) NOT NULL,
-  `factorE` int(11) NOT NULL,
-  `factorF` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `default_price` int(11) NOT NULL,
+  `picture` varchar(200) NOT NULL,
+  `bool_custom` tinyint(1) NOT NULL,
+  `bool_external` tinyint(1) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created` date DEFAULT NULL,
   `modified` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+
+--
+-- Dumping data for table `extras`
+--
+
+INSERT INTO `extras` (`id`, `name`, `description`, `default_price`, `picture`, `bool_custom`, `bool_external`, `category_id`, `user_id`, `created`, `modified`) VALUES
+(13, 'Cellar', 'Wet and dark', 1000, 'cellar.jpg', 0, 0, 2, 3, '2014-07-02', '2014-07-02'),
+(14, 'Jacuzzi', '^^', 1050, 'jacuzzi.jpg', 1, 0, 2, 4, '2014-07-02', '2014-07-02'),
+(15, 'Garden maze', 'Get lost!', 2000, 'maze.jpg', 1, 1, 3, 4, '2014-07-02', '2014-07-02'),
+(16, 'Something boring', 'Booooooooooooooring', 5, '', 1, 0, 1, 4, '2014-07-04', '2014-07-04'),
+(17, 'T-Rex', 'Rooooooooaarr', 1000, 'trex.jpg', 0, 1, 6, 6, '2014-07-04', '2014-07-04');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `floorplans`
+-- Table structure for table `extras_houses`
 --
 
-CREATE TABLE IF NOT EXISTS `floorplans` (
+CREATE TABLE IF NOT EXISTS `extras_houses` (
+  `extra_id` int(11) NOT NULL,
+  `house_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home_pictures`
+--
+
+CREATE TABLE IF NOT EXISTS `home_pictures` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `picture` varchar(50) NOT NULL,
-  `house_id` int(11) NOT NULL,
+  `picture` varchar(200) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created` date DEFAULT NULL,
-  `modified` int(11) DEFAULT NULL,
+  `modified` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `home_pictures`
+--
+
+INSERT INTO `home_pictures` (`id`, `picture`, `title`, `description`, `user_id`, `created`, `modified`) VALUES
+(5, 'home3.jpg', 'Gugen', 'Bilbao', 4, '2014-07-02', '2014-07-02'),
+(7, 'home2.jpg', 'weird house', 'lkdvn', 4, '2014-07-02', '2014-07-02'),
+(9, 'home1.png', 'Gehry something', 'png yes!', 4, '2014-07-02', '2014-07-02');
 
 -- --------------------------------------------------------
 
@@ -96,44 +186,53 @@ CREATE TABLE IF NOT EXISTS `floorplans` (
 
 CREATE TABLE IF NOT EXISTS `houses` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `notes` text,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
   `size` int(11) NOT NULL,
-  `stores` int(11) NOT NULL,
-  `type` int(11) NOT NULL,
+  `floors` int(11) NOT NULL,
   `price` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created` date DEFAULT NULL,
   `modified` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `houses`
 --
 
-INSERT INTO `houses` (`id`, `name`, `notes`, `size`, `stores`, `type`, `price`, `created`, `modified`) VALUES
-(1, 'Wayne''s Manor', 'Batcave not included', 300, 4, 3, 2000000, '2014-06-18', '2014-06-18'),
-(2, 'Batcave', 'Wayne Manor requiered', 500, 2, 50, 2147483647, '2014-06-18', '2014-06-18');
+INSERT INTO `houses` (`id`, `name`, `description`, `size`, `floors`, `price`, `type`, `user_id`, `created`, `modified`) VALUES
+(3, 'Wayne''s Manor', 'Beautiful palace. For a long time it has been home for the illustrious Wayne familie\r\n\r\nNote: Batcave not included', 2000, 2, 1234567890, 2, 6, '2014-07-01', '2014-07-01'),
+(4, 'Batcave', 'HQ of Batman\r\n\r\nNote: Waynes Manor not included', 1000, 2, 2147483647, 3, 3, '2014-07-01', '2014-07-01');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `i18n`
+-- Table structure for table `house_pictures`
 --
 
-CREATE TABLE IF NOT EXISTS `i18n` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `locale` varchar(6) NOT NULL,
-  `model` varchar(255) NOT NULL,
-  `foreign_key` int(10) NOT NULL,
-  `field` varchar(255) NOT NULL,
-  `content` text,
-  PRIMARY KEY (`id`),
-  KEY `locale` (`locale`),
-  KEY `model` (`model`),
-  KEY `row_id` (`foreign_key`),
-  KEY `field` (`field`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `house_pictures` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `picture` varchar(200) NOT NULL,
+  `house_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` date DEFAULT NULL,
+  `modified` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Dumping data for table `house_pictures`
+--
+
+INSERT INTO `house_pictures` (`id`, `name`, `description`, `picture`, `house_id`, `user_id`, `created`, `modified`) VALUES
+(7, 'Bat', 'sdnj', 'batcave.jpg', 4, 2, '2014-07-03', '2014-07-03'),
+(8, 'Batcave entry', 'sefsf', 'batcave2.jpg', 4, 2, '2014-07-03', '2014-07-03'),
+(9, 'pool', 'ksjdfnsn', 'cool.jpg', 3, 2, '2014-07-03', '2014-07-03'),
+(10, 'Front', 'sdgfgdfrgdf', 'wayne.jpg', 3, 2, '2014-07-03', '2014-07-03');
 
 -- --------------------------------------------------------
 
@@ -143,15 +242,33 @@ CREATE TABLE IF NOT EXISTS `i18n` (
 
 CREATE TABLE IF NOT EXISTS `lands` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
+  `name` varchar(200) NOT NULL,
   `notes` text NOT NULL,
-  `price` int(11) NOT NULL,
-  `costumer_id` int(11) NOT NULL,
+  `land_size` int(11) NOT NULL,
+  `land_price_per_m2` int(11) NOT NULL,
+  `dev_size` int(11) NOT NULL,
+  `dev_cost_per_m2` int(11) NOT NULL,
+  `notary_cost` int(11) NOT NULL,
+  `land_agent_cost` int(11) NOT NULL,
+  `land_tax` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `created` date DEFAULT NULL,
   `modified` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `lands`
+--
+
+INSERT INTO `lands` (`id`, `name`, `notes`, `land_size`, `land_price_per_m2`, `dev_size`, `dev_cost_per_m2`, `notary_cost`, `land_agent_cost`, `land_tax`, `customer_id`, `user_id`, `created`, `modified`) VALUES
+(1, 'Terminus', 'Desertic, no natural resources', 39999999, 1, 888888, 2, 30, 20, 21, 0, 3, '2014-07-02', '2014-07-02'),
+(2, 'Hyperion', 'Careful with the Shriek!', 2147483647, 3, 1234234, 2, 25, 23, 21, 0, 3, '2014-07-02', '2014-07-02'),
+(4, 'Alderaan', 'Set your house on an asteroid!', 3000, 1, 600, 30, 1, 1, 0, 0, 3, '2014-07-02', '2014-07-02'),
+(5, 'land for cus a2', '', 234, 120, 123, 123, 2, 3, 21, 2, 3, '2014-07-02', '2014-07-02'),
+(6, 'land cus b', '', 4567, 456789, 5678, 5678, 5, 6, 21, 3, 4, '2014-07-04', '2014-07-04'),
+(7, 'land cus b2', '', 5678, 567, 567, 5678, 3, 7, 21, 4, 4, '2014-07-04', '2014-07-04');
 
 -- --------------------------------------------------------
 
@@ -160,26 +277,24 @@ CREATE TABLE IF NOT EXISTS `lands` (
 --
 
 CREATE TABLE IF NOT EXISTS `proposals` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
-  `notes` text,
-  `created` date DEFAULT NULL,
-  `modified` date DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
-  `costumer_id` int(11) NOT NULL,
+  `notes` text NOT NULL,
+  `customer_id` int(11) NOT NULL,
   `land_id` int(11) NOT NULL,
   `house_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` date DEFAULT NULL,
+  `modified` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `proposals`
 --
 
-INSERT INTO `proposals` (`id`, `name`, `notes`, `created`, `modified`, `user_id`, `costumer_id`, `land_id`, `house_id`) VALUES
-(7, 'Big House', 'For Stefan', '2014-06-15', '2014-06-15', 2, 1, 1, 1),
-(8, 'Small house', 'For Jonatan', '2014-06-15', '2014-06-15', 2, 3, 2, 2),
-(11, 'A sales proposal', '', '2014-06-16', '2014-06-16', 4, 6, 3, 3);
+INSERT INTO `proposals` (`id`, `name`, `notes`, `customer_id`, `land_id`, `house_id`, `user_id`, `created`, `modified`) VALUES
+(2, 'Prop for cus b 1', '', 3, 4, 3, 4, '2014-07-04', '2014-07-04');
 
 -- --------------------------------------------------------
 
@@ -188,25 +303,29 @@ INSERT INTO `proposals` (`id`, `name`, `notes`, `created`, `modified`, `user_id`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `surname` varchar(200) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
   `created` date DEFAULT NULL,
   `modified` date DEFAULT NULL,
-  `role` int(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `created`, `modified`, `role`) VALUES
-(1, 'b4lrog', '0467e2ad120b7eba8d03de2f14152ee8ad29bdc1', '2014-06-01', '2014-06-12', 0),
-(2, 'Ricardo', '3bdbc7fecbbed14dc303ef207f84b63dff5649fc', '2014-06-04', '2014-06-12', 0),
-(3, '1234', '935dc0b6dcfaf42c9be2fa2b4b11ceeb59bf7492', '2014-06-12', '2014-06-12', 1),
-(4, 'a', '23befc93ce658cead70bf62c2eb177fc3f42c2c9', '2014-06-12', '2014-06-12', 2),
-(5, 'b', '5721e6c49c4039e4a78581633709b7ec3aeee276', '2014-06-12', '2014-06-12', 3);
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `name`, `surname`, `phone`, `email`, `created`, `modified`) VALUES
+(2, 'owner', '046d8cbb16e26dba35ddced9d4daa7b84468ef19', 1, 'O', 'wner', 1234689, 'ra@sfh.com', '2014-07-01', '2014-07-01'),
+(3, 'salea', '5a03b6bf4af5c6a37fdc095fd22269d415cc3943', 2, 'sale', 'a', 3467890, 'fghjkl@ghjk.de', '2014-07-01', '2014-07-01'),
+(4, 'saleb', '0afabdee6502c9fcefea4332883535d6e5c50cc0', 2, 'sale', 'b', 234567890, 'dfghjkl@dfghjkl.es', '2014-07-01', '2014-07-01'),
+(5, 'over', '6d0d62f180b14912b182b45e49fe55fbab9be1b6', 3, 'ov', 'er', 9876543, 'vjnhwhoiu@asuhcn.cad', '2014-07-01', '2014-07-01'),
+(6, 'elgatil', '3bdbc7fecbbed14dc303ef207f84b63dff5649fc', 0, 'Ricardo', 'Gomez', 345678, 'elgatil@gmail.com', '2014-07-01', '2014-07-01');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
