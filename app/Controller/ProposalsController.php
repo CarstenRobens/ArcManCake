d4a9622151ec2556c2fe7b3e174becbbfd345221
ricargoes <ricardogomezescalante@gmail.com>
2014-09-26 11:33:21 +0200

    -Downloading corrupt PDF fixed

--- app/Controller/ProposalsController.php
+++ app/Controller/ProposalsController.php
@@ -568,16 +568,21 @@ class ProposalsController extends AppController{
 		
 		$folder=__DIR__.'/../files/';
     	$file = $folder.$filename;
-    	debug($file);
     	
 		if (file_exists($file)) {
 			header('Content-Description: File Transfer');
 			header('Content-Type: application/octet-stream');
 			header('Content-Disposition: attachment; filename='.basename($file));
+			header('Content-Transfer-Encoding: binary');
+			header('Connection: Keep-Alive');
 			header('Expires: 0');
-			header('Cache-Control: must-revalidate');
+			header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 			header('Pragma: public');
 			header('Content-Length: ' . filesize($file));
+			
+			ob_end_flush();   // <--- instead of ob_clean()
+			
+			set_time_limit(0);
 			readfile($file);
 			exit;
 		}else{
